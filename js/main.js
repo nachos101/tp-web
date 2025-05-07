c58d47bdd444ae7d67c78f11d4249ccd792bbc5b
Luciano Sánchez Tomé <lucianostomeee@gmail.com>
2025-05-07 15:40:23 -0300

    added captcha

---
+++ js/main.js
@@ -0,0 +1,28 @@
+function generarCaptcha() {
+    let num1 = Math.floor(Math.random() * 100);
+    let num2 = Math.floor(Math.random() * 100);
+    
+    let captcha = num1 + num2; // Asigna el valor al campo de entrada
+    document.getElementById("captcha").innerHTML = captcha; // Muestra el captcha en el elemento HTML
+}
+
+function verificar(event) {
+    event.preventDefault();
+    let respuesta = document.getElementById("respuesta").value;
+    let captcha = document.getElementById("captcha").innerHTML;
+
+    if (respuesta == captcha){
+        document.getElementById("verificacion").innerHTML = "Captcha correcto ✅";
+    }
+    else {
+        document.getElementById("verificacion").innerHTML = "Captcha incorrecto ❌";
+        generarCaptcha();
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    generarCaptcha();
+
+    let boton_enviar = document.getElementById("Enviar");
+    boton_enviar.addEventListener("click", verificar);
+});
